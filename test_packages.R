4826a37ff82a782015bc92a9641015103f4f7fe0
tomme922 <simont9222@gmail.com>
2024-11-27 10:32:00 +0000

    sxas

--- test_packages.R
+++ test_packages.R
@@ -1,3 +1,8 @@
 library(httr2)
  # some comment, with extra explination
  #yeaheh
+library(cronR)
+
+cmd <- cron_rscript(rscript = "increment_one.R")
+cron_add(cmd, frequency = 'minutely', id = 'job1', description = 'Our first cronR job')
+cron_ls()
